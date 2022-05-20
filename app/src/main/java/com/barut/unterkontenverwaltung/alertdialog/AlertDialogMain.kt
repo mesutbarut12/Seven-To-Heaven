b4da743c8c30b7995f08e59405d4f8fcec3aa2f3
Mesut <mesutbarutprogrammieren@gmail.com>
2022-05-20 11:12:57 +0200

    Alert Dialog Klasse created
    Hier wurde die Klasse AlertDialog erstellt von wo man mit der OOP viele AlertDialog erstellen kann.

---
+++ app/src/main/java/com/barut/unterkontenverwaltung/alertdialog/AlertDialogMain.kt
@@ -0,0 +1,29 @@
+package com.barut.unterkontenverwaltung.alertdialog
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+
+class AlertDialogMain(val context: Context, val layout : Int) {
+
+    private lateinit var build : AlertDialog.Builder
+    private lateinit var created : AlertDialog
+
+    private fun setLayout() : View {
+        val view = LayoutInflater.from(context).inflate(layout,null)
+        build = AlertDialog.Builder(context)
+        build.setView(view)
+        return view
+    }
+    fun createDialog(){
+        setLayout()
+        created = build.create()
+        created.show()
+    }
+    fun cancelDialog(){
+        created.cancel()
+    }
+
+
+}
